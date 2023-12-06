58c32529a31b8042d89934b2376e233da6e9a708
Silas Schürger <schuergersilasdev@gmail.com>
2023-12-06 22:46:09 +0100

    ue5 init

---
+++ Source/TreeGrowthAi/TreeGrowthAi.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TreeGrowthAi.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TreeGrowthAi, "TreeGrowthAi" );
